629ebda06c394c8507117d786e098628c5f05280
Maddy Guthridge <hello@maddyguthridge.com>
2025-12-04 16:34:13 +1100

    Wait I forgot to commit the new files

---
+++ eslint.config.mjs
@@ -0,0 +1,148 @@
+// @ts-check
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
+import globals from 'globals';
+
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommendedTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
+  ...svelte.configs['flat/recommended'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        // Seemingly not defined in `globals.browser`
+        ScrollBehavior: 'readonly',
+      }
+    }
+  },
+  {
+    languageOptions: {
+      parser: ts.parser,
+      parserOptions: {
+        // ...
+        project: true,
+        extraFileExtensions: ['.svelte'], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+      },
+    },
+  },
+  {
+    files: ['**/*.svelte', '*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      // Default ESLint rules
+      // ====================
+
+      // Use `Promise.all` instead of `await` in a for loop for better async performance
+      'no-await-in-loop': 'error',
+      // Don't allow duplicate imports, because they are yucky
+      'no-duplicate-imports': 'error',
+      // Common mistake with `new Promise`
+      'no-promise-executor-return': ['error', { allowVoid: true }],
+      // Accidentally forgetting to use `back-ticks` for template literals
+      'no-template-curly-in-string': 'error',
+      // Use === instead of ==
+      'eqeqeq': 'error',
+      // Use dot notation for object property access
+      'dot-notation': 'error',
+      // Don't use `alert` and similar functions
+      'no-alert': 'error',
+      // Use camelCase for naming
+      'camelcase': 'error',
+      // Use `const` over `let` where reasonable
+      // Not required for destructuring, since that just makes things painful for Svelte props where
+      // some props are bindable
+      'prefer-const': ['error', { destructuring: 'all' }],
+
+      'no-restricted-imports': ['error', {
+        patterns: [
+          // Don't use legacy svelte features
+          { group: ['svelte/legacy'], message: 'Avoid legacy Svelte features' },
+          // Prevent importing from test files -- it causes tests to be registered multiple times,
+          // making pipelines slow
+          {
+            regex: '.*\\.test(.(t|j)s)?$',
+            message: (
+              'Do not import from test files. This causes tests to be run repeatedly. Instead, '
+              + 'move shared functionality to a separate non-test file.'),
+          },
+        ]
+      }],
+
+      // @typescript-eslint rules
+      // ========================
+
+      // Allow explicit any, to avoid type gymnastics
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        caughtErrors: 'none',
+      }],
+      // Disallow floating promises to avoid random crashes
+      '@typescript-eslint/no-floating-promises': 'error',
+      // Allow some `any` expressions since otherwise they seriously mess with tests, or enforce
+      // strictness in areas where it really doesn't matter (eg error handling)
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      // Also disable template expression checks, since they're also error handling stuff
+      // TODO: Enable them at some point when I get around to actually tidying things up
+      '@typescript-eslint/no-base-to-string': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      // FIXME: When I get around to hardening the request body validation, enable this rule again
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      // Allow empty functions, as they are useful to silence promise errors
+      '@typescript-eslint/no-empty-function': 'off',
+      // Use `type` instead of `interface`
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      // This error is already picked up by TypeScript, and it's annoying to need to silence it
+      // twice when it is incorrect
+      '@typescript-eslint/no-unsafe-call': 'off',
+
+      // Svelte (eslint-plugin-svelte) rules
+      // ===================================
+
+      'svelte/indent': ['error', { indent: 2 }],
+      'svelte/prefer-class-directive': ['error', {'prefer': 'empty'}],
+      'svelte/prefer-style-directive': 'error',
+      'svelte/spaced-html-comment': 'error',
+      // Disable no-navigation-without-resolve for the time being. I want to resolve these issues
+      // eventually, but for now, it is too much effort.
+      // TODO: Fix this in the future.
+      'svelte/no-navigation-without-resolve': 'off',
+      // Allow comments inside of mustaches in svelte syntax
+      "svelte/no-useless-mustaches": ["error", { "ignoreIncludesComment": true, "ignoreStringEscape": true }],
+    },
+  },
+  {
+    ignores: [
+      '**/.DS_Store',
+      '**/node_modules',
+      'build',
+      '.svelte-kit',
+      'package',
+      '**/.env',
+      '**/.env.*',
+      '!**/.env.example',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/yarn.lock',
+      '**/svelte.config.js',
+      '**/vitest.config.ts',
+      'eslint.config.mjs',
+      'vite.config.ts',
+    ],
+  },
+);
